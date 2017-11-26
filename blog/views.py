3a39c05bbec71a2f7bf9b582a179e98bfa20ae87
dako <dako@local>
2017-11-26 21:11:21 +0100

    Added publish and delete to post details

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -14,9 +15,26 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
 
 def post_new(request):
@@ -25,7 +43,8 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # Autopublish
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -40,9 +59,25 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # Autopublish
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def user_login(request):
+    item = 'user_login'
+    return render(request, 'blog/todo.html', {'item': item})
+
+
+def user_logout(request):
+    item = 'user_logout'
+    return render(request, 'blog/todo.html', {'item': item})
+
+
+def user_account(request):
+    # user = User.objects.filter(username=request.user)
+    return render(request, 'blog/user_account.html')
